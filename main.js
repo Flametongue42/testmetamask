a6121deae9d364527764e2a89bcd3a7419db7317
Flametongue <102842241+Flametongue42@users.noreply.github.com>
2022-04-04 01:12:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ console.log("connect Metamask");
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 await provider.send("eth_requestAccounts", []);
 const signer=provider.getSigner();
+console.log(signer);
 //await checkJPYC(provider);
   
  //matic JPYC adrress
